49489ae8ab2652413dcc0447c43307fa8cf6e59e
Peter Kurzok <github@peterkurzok.de>
2018-06-12 11:12:35 +0200

    Set Java Vertsion

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -4,6 +4,7 @@ export EDITOR='nano'
 export NVM_DIR="/Users/pkurzok/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" 
 
+export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
 export ANT_HOME=~/DIP/apache-ant-1.8.4
 export ANT_ARGS='-propertyfile ~/DIP/ant.properties'
 export ECLIPSE_TARGET=~/DIP/eclipse-target
