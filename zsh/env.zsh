bd9e7b118aa7f66ad40589c5c69ca5145f9495b8
Peter Kurzok <github@peterkurzok.de>
2018-09-24 11:34:51 +0200

    Added Fastlane Default user for xcversion

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -4,7 +4,7 @@ export EDITOR='nano'
 export NVM_DIR="/Users/pkurzok/.nvm"
 . "/usr/local/opt/nvm/nvm.sh" 
 
-export JAVA_HOME=`/usr/libexec/java_home -v 1.7`
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 export ANT_HOME=~/DIP/apache-ant-1.8.4
 export ANT_ARGS='-propertyfile ~/DIP/ant.properties'
 export ECLIPSE_TARGET=~/DIP/eclipse-target
@@ -12,4 +12,6 @@ export ECLIPSE_TARGET=~/DIP/eclipse-target
 export PATH=$PATH:$ANT_HOME/bin
 
 export PATH="$HOME/.fastlane/bin:$PATH"
+
+export FASTLANE_USER="appleci@prodyna.com"
 
