613cdf8ed4776d2dd9ebc291da1ce6e209c0f4ce
Peter Kurzok <peter.kurzok@prodyna.com>
2020-05-28 13:55:14 +0200

    Env update

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,10 +1,10 @@
 export EDITOR='nano'
 
 # This loads nvm
-export NVM_DIR="/Users/pkurzok/.nvm"
-. "/usr/local/opt/nvm/nvm.sh" 
+#export NVM_DIR="/Users/pkurzok/.nvm"
+#. "/usr/local/opt/nvm/nvm.sh" 
 
-export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+#export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 export ANT_HOME=~/DIP/apache-ant-1.8.4
 export ANT_ARGS='-propertyfile ~/DIP/ant.properties'
 export ECLIPSE_TARGET=~/DIP/eclipse-target
@@ -15,3 +15,4 @@ export PATH="$HOME/.fastlane/bin:$PATH"
 
 export FASTLANE_USER="appleci@prodyna.com"
 
+export GRADLE_USER_HOME=~/.gradle
